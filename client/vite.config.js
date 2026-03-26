25570d61c2f3afccbf73332dd2c7dbaa7c24ae99
Luis Zeballos <luis.futuro.01@gmail.com>
2026-03-25 22:00:12 -0400

    vite config correction

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000', // Apunta a tu backend Node
+        target: 'http://localhost:4000', 
         changeOrigin: true,
         secure: false,
       },
     },
-    host: "0.0.0.0",
-    port: 5173
   },
 });
